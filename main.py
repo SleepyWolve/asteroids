4b06c37380b84a1f5e72359d8945b492b0589830
SleepyWolve <83551917+SleepyWolve@users.noreply.github.com>
2025-02-18 20:52:40 +0200

    add Asteroid class and logic to draw them to the screen

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import pygame
 from constants import *
-from player import * 
+from player import *
+from asteroidfield import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()  
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -18,10 +29,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        #player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+        #player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
